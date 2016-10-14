3068704215bbfe2b3c11b2c1280aaeedfaa3bc64
Arthur Guiot <arguiot@gmail.com>
2016-10-14 18:05:18 +1100

    Update and rename image.php to image-generator.php

--- image-generator.php
+++ image-generator.php
@@ -8,7 +8,7 @@ $bleuclair = imagecolorallocate($image, 156, 227, 254);
 $noir = imagecolorallocate($image, 0, 0, 0);
 $blanc = imagecolorallocate($image, 255, 255, 255);
 
-imagestring($image, 160, 1400, 450, "Salut ici The Scientist !", $blanc);
+imagestring($image, 160, 1400, 450, "Hey !", $blanc);
 
 imagepng($image);
 ?>
