6257ed46e13a6db2b8368419b6c700a6255fc051
WajahatAnwar <zubair@ikarobar.com>
2017-10-24 03:36:41 +0500

    beautification of the code

--- sum.php
+++ sum.php
@@ -2,31 +2,35 @@
 if (!isset($_GET['i']))
 {
 ?>
-<form method="GET">
-<input type="text" name="i" >
-<input type="submit">
-</form>
+	<form method="GET">
+		<input type="text" name="i" >
+		<input type="submit">
+	</form>
 <?php
 }
 else
 {
 ?>
-<form method="GET">
-<input type="text" name="i" >
-<input type="submit">
-</form>
+	<form method="GET">
+		<input type="text" name="i" >
+		<input type="submit">
+	</form>
 <?php
 $time_start = microtime(true); 
+
 if (isset($_GET['i'])){
-$number = (int) $_GET['i'];	
+	$number = (int) $_GET['i'];	
 } else {
 	die ('Error: Argument not specified.');
 }
+
 echo (($number * $number) + $number)/2
 echo 1 . "\n";
+
 //Bam! Finished.
 $time_end = microtime(true);
 $total = ($time_end - $time_start)/60;
 echo "Operation completed in $cycles cycles and it took $total seconds.";
+
 }
 ?>
